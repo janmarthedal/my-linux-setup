c3e68bbd7b7e84aa24ee8d85f353d7ab504ec46b
Jan Marthedal Rasmussen <jan@janmr.com>
2024-06-29 20:05:40 +0200

    Install alacritty

---
+++ install/app-alacritty.sh
@@ -0,0 +1,5 @@
+sudo apt install -y alacritty
+mkdir -p ~/.config/alacritty
+cp ~/.local/share/my-linux-setup/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+cp ~/.local/share/my-linux-setup/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
+cp ~/.local/share/my-linux-setup/configs/alacritty/font.toml ~/.config/alacritty/font.toml
