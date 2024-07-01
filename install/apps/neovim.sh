2651bf2b6768dc6555901885e652059617b02f68
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-01 21:39:02 +0200

    Install neovim colorscheme

--- install/apps/neovim.sh
+++ install/apps/neovim.sh
@@ -2,6 +2,8 @@ sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	cp ~/.local/share/my-linux-setup/themes/neovim/catppuccin-latte.lua ~/.config/nvim/lua/plugins/theme.lua
+
 	echo "** LazyVim ** It is recommended to run :LazyHealth after installation."
 	echo "** LazyVim ** This will load all plugins and check if everything is working correctly."
 fi
