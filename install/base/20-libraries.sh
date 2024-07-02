fc4046bf9f8cd69b1d63e77045a3ee4872655dad
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-02 12:46:38 +0200

    Move some library installs to mise.sh

--- install/base/20-libraries.sh
+++ install/base/20-libraries.sh
@@ -1,8 +1,3 @@
 sudo apt install -y \
 	build-essential pkg-config autoconf \
 	libssl-dev zlib1g-dev
-
-# These are necessary for building python
-sudo apt install -y \
-	libffi-dev libreadline-dev libncurses-dev \
-	libbz2-dev libsqlite3-dev tk-dev liblzma-dev
