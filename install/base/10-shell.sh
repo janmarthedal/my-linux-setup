c7c7e2996e3a943e72bf13e3f095978d3d221507
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-03 15:32:55 +0200

    Add inputrc file as part of base/shell install

--- install/base/10-shell.sh
+++ install/base/10-shell.sh
@@ -3,5 +3,5 @@ mkdir -p ~/.config/my-linux-setup/bash/rc
 cp ~/.local/share/my-linux-setup/configs/bash/bashrc ~/.bashrc
 cp ~/.local/share/my-linux-setup/configs/bash/rc/*.sh ~/.config/my-linux-setup/bash/rc/
 
-# [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
-# cp ~/.local/share/my-linux-setup/configs/inputrc ~/.inputrc
+[ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
+cp ~/.local/share/my-linux-setup/configs/inputrc ~/.inputrc
