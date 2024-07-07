97271eee00587ce3f9f6c6cc019e5236878afdbf
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-07 16:42:25 +0200

    Set gnome theme

--- themes/set-theme.sh
+++ themes/set-theme.sh
@@ -1,18 +1,18 @@
 # THEME must be set
 
 if [ -n "$THEME" ]; then
-  THEME_SLUG=$(echo $THEME | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
+  THEME_DIR=$(echo $THEME | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 
   if [ -d ~/.config/alacritty/theme.toml ]; then
-    cp ~/.local/share/my-linux-setup/themes/$THEME_SLUG/alacritty.toml ~/.config/alacritty/theme.toml
+    cp ~/.local/share/my-linux-setup/themes/$THEME_DIR/alacritty.toml ~/.config/alacritty/theme.toml
   fi
   if [ -d ~/.config/nvim/lua/plugins/theme.lua ]; then
-    cp ~/.local/share/my-linux-setup/themes/$THEME_SLUG/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+    cp ~/.local/share/my-linux-setup/themes/$THEME_DIR/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
   fi
   if [ -f ~/.wezterm.lua ]; then
-    sed -i 's/config.color_scheme =.*/config.color_scheme = "$THEME"/g' ~/.wezterm.lua
+    sed -i "s/config.color_scheme =.*/config.color_scheme = \"$THEME\"/g" ~/.wezterm.lua
   fi
 
-  # source $~/.local/share/my-linux-setup/themes/$THEME/gnome.sh
-  # source $~/.local/share/my-linux-setup/themes/$THEME/vscode.sh
+  source ~/.local/share/my-linux-setup/themes/$THEME_DIR/gnome.sh
+  # source ~/.local/share/my-linux-setup/themes/$THEME/vscode.sh
 fi
