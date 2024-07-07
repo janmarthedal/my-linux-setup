77763761294a4c11755dcd3a22d2987efbab3de5
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-07 12:03:16 +0200

    Move theme files

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -1,5 +1,5 @@
 sudo apt install -y alacritty
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/my-linux-setup/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
-cp ~/.local/share/my-linux-setup/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
+cp ~/.local/share/my-linux-setup/themes/cattpuccin-latte/alacritty.toml ~/.config/alacritty/theme.toml
 cp ~/.local/share/my-linux-setup/configs/alacritty/font.toml ~/.config/alacritty/font.toml
