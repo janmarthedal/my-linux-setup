3eef1f2b7321b0ba6ff2d0e1be0c419980a01cf9
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-07 11:54:01 +0200

    Move neovim theme file

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,12 +1,12 @@
 sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	# Disable update notification popup in starter config
-	sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
-	# Set color theme
-	cp ~/.local/share/my-linux-setup/themes/neovim/catppuccin-latte.lua ~/.config/nvim/lua/plugins/theme.lua
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  # Disable update notification popup in starter config
+  sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
+  # Set color theme
+  cp ~/.local/share/my-linux-setup/themes/catppuccin-latte/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
-	echo "** LazyVim ** It is recommended to run :LazyHealth after installation."
-	echo "** LazyVim ** This will load all plugins and check if everything is working correctly."
+  echo "** LazyVim ** It is recommended to run :LazyHealth after installation."
+  echo "** LazyVim ** This will load all plugins and check if everything is working correctly."
 fi
