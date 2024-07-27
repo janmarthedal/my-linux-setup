de8f7c7e2d571fad60219412d3e70298fab6874b
Jan Marthedal Rasmussen <jan@janmr.com>
2024-07-27 10:08:14 +0200

    Install neovim using tarball instead of apt

--- install/rust-analyzer.sh
+++ install/rust-analyzer.sh
@@ -1,5 +1,5 @@
 rustup component add rust-analyzer
 
 if [[ -d $HOME/.config/nvim ]]; then
-  cp ~/.local/share/my-linux-setup/configs/rustaceanvim.lua ~/.config/nvim/lua/plugins/
+  cp $MY_LINUX_SETUP_PATH/configs/rustaceanvim.lua ~/.config/nvim/lua/custom/plugins/
 fi
