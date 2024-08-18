d9d781856ee311bd74f5a60c0474aaa2671ef704
Jan Marthedal Rasmussen <jan@janmr.com>
2024-08-18 11:17:12 +0200

    Get dropbox pkg from homepage

--- install/dropbox.sh
+++ install/dropbox.sh
@@ -1,2 +1,7 @@
-sudo apt install -y nautilus-dropbox
-gtk-launch dropbox.desktop
+cd /tmp
+curl -fLo dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2024.04.17_amd64.deb
+sudo dpkg -i dropbox.deb
+nautilus --quit
+rm dropbox.deb
+cd -
+
