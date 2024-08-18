3046448662c4a192ddab1ed7613cd02c44aefdb6
Jan Marthedal Rasmussen <jan@janmr.com>
2024-08-18 11:32:21 +0200

    Install obsidian using deb from homepage

--- install/obsidian.sh
+++ install/obsidian.sh
@@ -1,1 +1,6 @@
-sudo apt install -y obsidian
+cd /tmp
+curl -fLo obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb
+sudo dpkg -i obsidian.deb
+rm obsidian.deb
+cd -
+
